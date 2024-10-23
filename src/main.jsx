fef29598795bbaeac0636b0879b374449f31db39
Your Name~edmar <you@example.com>
2024-10-23 19:46:21 -0300

    testanto html puro

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,12 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+  <h1>
+    Hello, world!
+  </h1>
+  
+  // <StrictMode>
+    // <App />
+  // </StrictMode>,
 )
