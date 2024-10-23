bac82034fc1ec89670a32653653a43da7d6f01bb
Your Name~edmar <you@example.com>
2024-10-23 19:58:56 -0300

    segundo commit

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,7 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-
-  <h1>
-    Hello, world!
-  </h1>
-  
-  // <StrictMode>
-    // <App />
-  // </StrictMode>,
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
